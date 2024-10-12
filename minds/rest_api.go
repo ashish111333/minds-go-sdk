b25b7b241042d9f7f712a5dcbe832e39ad5edb87
ashish111333 <ashishkumar0265aaa@gmail.com>
2024-10-12 15:38:45 +0530

    modified rest_api and added datasource package

--- minds/rest_api.go
+++ minds/rest_api.go
@@ -1,13 +1,12 @@
 package minds
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
 	"time"
-
-	
 )
 
 var (
@@ -35,7 +34,7 @@ func NewRestApi(apiKey, baseUrl string) *RestApi {
 		Client:  &http.Client{Timeout: time.Second * 10},
 	}
 
-};
+}
 
 // makes http requests for GET,POST,DELETE,PATCH methods
 func (api *RestApi) MakeHttpRequest(httpMethod, url string, RequestData interface{}) (*http.Response, error) {
@@ -47,26 +46,44 @@ func (api *RestApi) MakeHttpRequest(httpMethod, url string, RequestData interfac
 
 		jsonData, err = json.Marshal(RequestData)
 		if err != nil {
-			return nil,fmt.Errorf("Failed to marshal json: %w",err)
-	
+			return nil, fmt.Errorf("failed to marshal json: %w", err)
+
+		}
 	}
 
-	//make request
-	request, err := http.NewRequest(httpMethod, api.baseUrl+url)
+	//create request
+	request, err := http.NewRequest(httpMethod, api.BaseUrl+url, bytes.NewBuffer(jsonData))
 	if err != nil {
 
 		return nil, fmt.Errorf("failed to create http request: %w", err)
 
 	}
+	//Set Headers
 
+	request.Header.Set("Authorization", "Bearer "+api.ApiKey)
+	request.Header.Set("Application-type")
+	// make request
 	response, err := api.Client.Do(request)
 	if err != nil {
 
-		return nil, fmt.Errorf("error in http response : %w",err);
+		return nil, fmt.Errorf("error in http response : %w", err)
 	}
-	return response,nil;
+	return response, nil
 
 }
 
+func (api *RestApi) Get() (*http.Response, error) {
 
+}
 
+func (api *RestApi) Delet() (*http.Response, error) {
+
+}
+
+func (api *RestApi) Post() (*http.Response, error) {
+
+}
+
+func (api *RestApi) Patch() (*http.Response, error) {
+
+}
