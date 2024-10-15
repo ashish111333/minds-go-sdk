f32d8d7c5228541e6d054b0df9d186fe30947536
ashish111333 <ashishkumar0265aaa@gmail.com>
2024-10-15 10:09:17 +0530

    updates

--- minds/client.go
+++ minds/client.go
@@ -1,4 +1,15 @@
 package minds
 
+import (
+	"github.com/ashish111333/minds-go-sdk/minds"
+	"github.com/ashish111333/minds-go-sdk/minds/datasources"
+)
+
 type Client struct {
+	Api         *minds.RestApi
+	Datasources datasources.DataSources
+}
+
+func NewClient() {
+
 }
--- minds/datasources/datasources.go
+++ minds/datasources/datasources.go
@@ -1,4 +1,32 @@
 package datasources
 
+import "github.com/ashish111333/minds-go-sdk/minds"
+
 type DatabaseConfig struct {
+	Name           string            `json:"name"`
+	Engine         string            `json:"engine"`
+	Description    string            `json:"descriptiong"`
+	ConnectionData map[string]string `json:"connection_data,omitempty"`
+}
+
+type DataSource struct {
+	DatabaseConfig
+}
+
+type DataSources struct {
+	client minds.Client
+}
+
+func () create() {
+
+}
+func () list() {
+
+}
+func () get() {
+
+}
+
+func () drop() {
+
 }
