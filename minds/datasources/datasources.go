75e79e8f37de0c3a6e0f9fe8152e2169ca749c56
ashish111333 <ashishkumar0265aaa@gmail.com>
2024-10-16 00:35:33 +0530

    changes to datasources.go

--- minds/datasources/datasources.go
+++ minds/datasources/datasources.go
@@ -1,12 +1,17 @@
 package datasources
 
-import "github.com/ashish111333/minds-go-sdk/minds"
+import (
+	"log"
+
+	"github.com/ashish111333/minds-go-sdk/minds"
+)
 
 type DatabaseConfig struct {
 	Name           string            `json:"name"`
 	Engine         string            `json:"engine"`
-	Description    string            `json:"descriptiong"`
+	Description    string            `json:"description"`
 	ConnectionData map[string]string `json:"connection_data,omitempty"`
+	Tables         []string          `json:"tables"`
 }
 
 type DataSource struct {
@@ -14,19 +19,33 @@ type DataSource struct {
 }
 
 type DataSources struct {
-	client minds.Client
+	Api minds.RestApi
 }
 
-func () create() {
+func (d *DataSources) create(DsConfig *DatabaseConfig, replace bool) {
+	name = DsConfig.Name
+
+	d.Api.Post("/datasources", DsConfig)
 
 }
-func () list() {
+func (d *DataSources) list() {
 
 }
-func () get() {
+func (d *DataSources) get(name string) DataSource {
+	log.Printf("Making Get request Url:%s", "/datasources"+name)
+	data, err := d.Api.Get("/datasources"+name, nil)
+	if err != nil {
+		log.Printf("failed to get datasource :", err)
+	}
 
 }
 
-func () drop() {
+func (d *DataSources) drop(name string) {
+
+	log.Printf("Making Delete request Url:%s", "/datasources/"+name)
+	_, err := d.Api.Delete("/datasources/"+name, nil)
+	if err != nil {
+		log.Printf("failed to delete Datasource: ", err)
+	}
 
 }
