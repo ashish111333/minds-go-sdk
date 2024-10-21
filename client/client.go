90e5641ea8ce33cf03a54393e01e92048d3c459e
ashish111333 <ashishkumar0265aaa@gmail.com>
2024-10-21 18:13:01 +0530

    changes to minds.go

--- client/client.go
+++ client/client.go
@@ -1,9 +1,23 @@
-package minds
+package client
+
+import (
+	"github.com/ashish111333/minds-go-sdk/api"
+	"github.com/ashish111333/minds-go-sdk/datasources"
+	"github.com/ashish111333/minds-go-sdk/minds"
+)
 
 type Client struct {
-	Api
+	Api         *api.RestApi
+	Datasources *datasources.DataSources
+	Minds       *minds.Minds
 }
 
-func NewClient() {
+func NewClient() *Client {
+
+	return &Client{
+		Api:         api.NewRestApi(),
+		Datasources: 
+		Minds: 
+	}
 
 }
--- minds/minds.go
+++ minds/minds.go
@@ -1,23 +1,22 @@
 package minds
 
 import (
+	"log"
+
+	"github.com/ashish111333/minds-go-sdk/api"
 	"github.com/ashish111333/minds-go-sdk/datasources"
 )
 
 type Mind struct {
-	ModeName  string
-	provider  string
-	CreatedAt string
-	UpdatedAt string
-	Parameters
-	datasources *datasources.DataSources
+	ModeName    string
+	Provider    string
+	CreatedAt   string
+	UpdatedAt   string
+	Parameters  map[string]interface{}
+	Datasources *datasources.DataSources
 }
 
-func NewMind(name, modelName, provider) {
-
-}
-
-func (mind *Mind) Update() {
+func (mind *Mind) Update(name, modelName, promptTemplate string, params map[string]interface{}) {
 
 }
 
@@ -30,5 +29,44 @@ func (mind *Mind) DeleteDatasource() {
 }
 
 func (mind *Mind) Completion() {
+
+}
+
+type Minds struct {
+	api api.RestApi
+
+	project string
+}
+
+func NewMinds() *Minds {
+
+	return &Minds{}
+}
+
+func (minds *Minds) Create() {
+
+}
+
+func (minds *Minds) Drop(name string) {
+	_, err := minds.api.Delete("/projects/"+minds.project+"/minds/"+name, nil)
+	if err != nil {
+		log.Printf("failed to delete mind : %v \n", err)
+	}
+
+}
+
+func (minds *Minds) List() {
+	data, err := minds.api.Get("/projects/"+minds.project+"/minds", nil)
+	if err != nil {
+		log.Printf("failed to get minds list : %v \n", err)
+	}
+
+}
+
+func () Get(name string) {
+
+}
+
+func () checkDatasource() {
 
 }
